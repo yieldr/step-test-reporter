671ba92e6354209d1ee502e9c3a6f80d4af5f63d
Alex Kalyvitis <alex.kalyvitis@gmail.com>
2018-01-15 13:21:01 +0100

    add command argument

--- README.md
+++ README.md
@@ -2,18 +2,33 @@
 
 This step allows the user to send test reports to [Code Climate](https://codeclimate.com/).
 
-# Options:
+# Commands:
 
-- `id` (optional) Your repo's test reporter id. Read [here](https://docs.codeclimate.com/docs/finding-your-test-coverage-token) on how to find yours.
-- `coverage-endpoint` (optional) Use this to report coverage to your locally-hosted Code Climate: Enterprise instance.
+- `after-build`     Locate, parse, and re-format supported coverage sources. Upload pre-formatted coverage payloads to Code Climate servers.
+- `before-build`    To be run before a build
+- `env`             Infer and output information about the environment the reporter is running in.
+- `format-coverage` Locate, parse, and re-format supported coverage sources.
+- `help`            Help about any command
+- `sum-coverage`    Combine (sum) multiple pre-formatted coverage payloads into one.
+- `upload-coverage` Upload pre-formatted coverage payloads to Code Climate servers.
+
+# Args:
+
+- `batch-size`          (optional, default 500) Batch size for source files (default 500)
+- `coverage-endpoint`   (optional, default "https://api.codeclimate.com/v1/test_reports") Endpoint to upload coverage information to
+- `coverage-input-type` (optional) Type of input source to use [clover, cobertura, coverage.py, excoveralls, gcov, gocov, jacoco, lcov, simplecov]
+- `id`                  (optional) Your repo's test reporter identifier. Read [here](https://docs.codeclimate.com/docs/finding-your-test-coverage-token) on how to find yours.
+- `prefix`              (optional) The root directory where the coverage analysis was performed
 
 # Example
 
 ```
 deploy:
 	steps:
-	  - yieldr/test-reporter@0.4.2
+	  - yieldr/test-reporter@0.4.2:
 	  		id: pydjvVvCgIEyKOgIakHyYPy3QyN6B5mbW7PfocaCv5qxvPzJpXf1TdqEZdGqFbs
+	  		coverage-input-type: gocov
+	  		command: after-build
 ```
 
 # License
--- run.sh
+++ run.sh
@@ -5,19 +5,43 @@ test_reporter="$WERCKER_STEP_ROOT/test-reporter"
 main() {
   "$test_reporter" version --client
 
+  cmd="$WERCKER_TEST_REPORTER_COMMAND"
+
+  if [ -z "$WERCKER_TEST_REPORTER_COMMAND" ]; then
+    cmd="after-build"
+  fi
+
   args=
 
-  if [ -z "$WERCKER_TEST_REPORTER_ID" ]; then
-    echo "WERCKER_TEST_REPORTER_ID=$WERCKER_TEST_REPORTER_ID"
-    args="--id=\"$WERCKER_TEST_REPORTER_ID\""
-  fi
+  if [ -n "$WERCKER_TEST_REPORTER_BATCH_SIZE" ]; then
+    echo "WERCKER_TEST_REPORTER_BATCH_SIZE=$WERCKER_TEST_REPORTER_BATCH_SIZE"
+    args="$args --batch-size=\"$WERCKER_TEST_REPORTER_BATCH_SIZE\""
 
-  if [ -z "$WERCKER_TEST_REPORTER_COVERAGE_ENDPOINT" ]; then
+  if [ -n "$WERCKER_TEST_REPORTER_COVERAGE_ENDPOINT" ]; then
     echo "WERCKER_TEST_REPORTER_COVERAGE_ENDPOINT=$WERCKER_TEST_REPORTER_COVERAGE_ENDPOINT"
     args="$args --coverage-endpoint=\"$WERCKER_TEST_REPORTER_COVERAGE_ENDPOINT\""
-  fi
 
-  eval "$test_reporter" "$args"
+  if [ -n "$WERCKER_TEST_REPORTER_COVERAGE_INPUT_TYPE" ]; then
+    echo "WERCKER_TEST_REPORTER_COVERAGE_INPUT_TYPE=$WERCKER_TEST_REPORTER_COVERAGE_INPUT_TYPE"
+    args="$args --coverage-input-type=\"$WERCKER_TEST_REPORTER_COVERAGE_INPUT_TYPE\""
+
+  if [ -n "$WERCKER_TEST_REPORTER_EXIT_CODE" ]; then
+    echo "WERCKER_TEST_REPORTER_EXIT_CODE=$WERCKER_TEST_REPORTER_EXIT_CODE"
+    args="$args --exit-code=\"$WERCKER_TEST_REPORTER_EXIT_CODE\""
+
+  if [ -n "$WERCKER_TEST_REPORTER_ID" ]; then
+    echo "WERCKER_TEST_REPORTER_ID=$WERCKER_TEST_REPORTER_ID"
+    args="$args --id=\"$WERCKER_TEST_REPORTER_ID\""
+
+  if [ -n "$WERCKER_TEST_REPORTER_PREFIX" ]; then
+    echo "WERCKER_TEST_REPORTER_PREFIX=$WERCKER_TEST_REPORTER_PREFIX"
+    args="$args --prefix=\"$WERCKER_TEST_REPORTER_PREFIX\""
+
+  if [ -n "$WERCKER_TEST_REPORTER_DEBUG" ]; then
+    echo "WERCKER_TEST_REPORTER_DEBUG=$WERCKER_TEST_REPORTER_DEBUG"
+    args="$args --debug=\"$WERCKER_TEST_REPORTER_DEBUG\""
+
+  eval "$test_reporter" "$cmd" "$args"
 }
 
 main
